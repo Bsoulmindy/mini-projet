684f85ef00760092e093f2d2da7f927ad45aa231
Bsoulmindy <oussamabader@gmail.com>
2022-04-07 09:28:23 +0000

    t

--- src/main/java/com/terroir/controllers/ProduitController.java
+++ src/main/java/com/terroir/controllers/ProduitController.java
@@ -17,22 +17,22 @@ public class ProduitController {
     @Autowired
 	ProduitService produitService;
 
-	@RequestMapping("/produits/")
-	public Produit getProduit(@PathVariable("id") int id){
-        return produitService.getProduit(id);
-    }
+	// @RequestMapping("/produits/")
+	// public Produit getProduit(@PathVariable("id") int id){
+    //     return produitService.getProduit(id);
+    // }
  
-    public List<Integer> getProduiPartMatP(@RequestParam("idmatps")int...idmatps){
-        return produitService.getListProduitsParMatiers(idmatps);
-    }
+    // public List<Integer> getProduiPartMatP(@RequestParam("idmatps")int...idmatps){
+    //     return produitService.getListProduitsParMatiers(idmatps);
+    // }
  
-    public List<Integer> getProduitParOrigine(@RequestParam("idorigine")int idorigine){
-        return produitService.getListProduitsParOrigine(idorigine);
-    }
+    // public List<Integer> getProduitParOrigine(@RequestParam("idorigine")int idorigine){
+    //     return produitService.getListProduitsParOrigine(idorigine);
+    // }
  
-    public List<Integer> getProduitParCat(@RequestParam("idcat")int idcat){
-        return produitService.getListProduitsParCat(idcat);
-    }
+    // public List<Integer> getProduitParCat(@RequestParam("idcat")int idcat){
+    //     return produitService.getListProduitsParCat(idcat);
+    // }
 
 
 }
